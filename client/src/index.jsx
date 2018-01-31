6c7227ef23d7cdd6c3ed54d2c3a60ad81900b276
Daniel Sanchez <atlow.archive@gmail.com>
2018-01-30 20:44:31 -0800

    [react-router] Provisional setup

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -1,30 +1,33 @@
 import React from 'react';
-import { render } from 'react-dom';
+import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
-import { Router, Route, IndexRoute, browserHistory } from 'react-router';
+import { Router, Route, IndexRoute, browserHistory, ConnectedRouter } from 'react-router';
 
 // Import styles
 import './index.css'
 
 // Import components
-import Home from './components/LandingPage/Home';
-import Search from './components/Search/SearchResultPage';
-import ListingPage from './components/Listings/ListingPage';
-import BookingForm from './components/Listings/BookingForm';
-import UserPage from "./components/User/UserPage"
+import App from './App.jsx';
+import Home from './components/LandingPage/Home.jsx';
+import Search from './components/Search/SearchResultPage.jsx';
+import ListingPage from './components/Listings/ListingPage.jsx';
+import BookingForm from './components/Listings/BookingForm.jsx';
+import UserPage from "./components/User/UserPage.jsx"
 
 
 const router = (
-  <Router history={browserHistory}>
-    <Route path="/" component={App}>
-      <IndexRoute component={Home}></IndexRoute>
-      <Route path="/search" component={Search}></Route>
-      {/* Setup :listingId to link to specific listing */}
-      <Route path="/listings/:listingId" component={ListingPage}></Route>
-      <Route path="/listing/booking:listingId" component={BookingForm}></Route>
-      <Route path="/user/:userId" component={UserPage}></Route>
-    </Route>
-  </Router>
+  <ConnectedRouter history={browserHistory}>
+    <div>
+      <Route path="/" component={App}>
+        {/* <IndexRoute component={Home}></IndexRoute> */}
+        {/* <Route path="/search" component={Search}></Route> */}
+        {/* Setup :listingId/:userId to link to specific listing/user */}
+        {/* <Route path="/listings/:listingId" component={ListingPage}></Route>
+        <Route path="/listing/booking:listingId" component={BookingForm}></Route>
+        <Route path="/user/:userId" component={UserPage}></Route> */}
+      </Route>
+    </div>
+  </ConnectedRouter>
 );
 
-render(router, document.getElementById('app'));
+ReactDOM.render(router, document.getElementById('app'));
