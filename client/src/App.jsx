bc118f287b6a709130627e758e3d6e50ec5cb48a
Daniel Sanchez <atlow.archive@gmail.com>
2018-02-01 17:08:38 -0800

    [routing] Persist Navbar on / route

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,30 +2,33 @@ import React from 'react';
 import { Route, Switch, BrowserRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
 
-import Home from './components/LandingPage/Home.jsx';
-import Search from './components/Search/SearchResultPage.jsx';
-import ListingPage from './components/Listings/ListingPage.jsx';
-import BookingForm from './components/Listings/BookingForm.jsx';
-import UserPage from "./components/User/UserPage.jsx"
+import Home from './components/LandingPage/Home';
+import SearchResults from './components/Search/SearchResultPage';
+import ListingPage from './components/Listings/ListingPage';
+import BookingForm from './components/Listings/BookingForm';
+import UserPage from "./components/User/UserPage"
 import Login from './components/Auth/Login';
 import SignUp from './components/Auth/SignUp';
+import NavBar from './components/Nav/NavBar';
 
 const App = ({ store }) => {
   return (
     <Provider store={store}>
       <BrowserRouter>
         <div>
+          <Route path="/" component={NavBar} />
           <Switch>
             <Route path="/login" component={Login} />
             <Route path="/signup" component={SignUp} />
-            <Route path='/search' component={Search} />
+            {/* Pass query into search request */}
+            <Route path='/search/:query' component={SearchResults} />
             {/* Setup :listingId for specific listing */}
             <Route path='/listing/book/:listingId' component={BookingForm} />
             <Route path='/listing/:listingId' component={ListingPage} />
             {/* Setup :userId for specific user */}
             <Route path='/user/:userId' component={UserPage} />
             <Route path='/' component={Home} />
-            </Switch>
+          </Switch>
         </div>
       </BrowserRouter>
     </Provider>
