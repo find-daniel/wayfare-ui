9d7041c9525815b244b5502e25fb2173c32bdff1
Eric Carmona <ericscarmona@yahoo.com>
2018-02-06 15:48:18 -0800

    [client] rendering messages using sockets

--- client/src/components/User/Messages.jsx
+++ client/src/components/User/Messages.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import 'babel-polyfill';
 import { connect } from 'react-redux';
 import { bindActionCreators } from 'redux'; 
 import io from 'socket.io-client';
@@ -10,7 +11,6 @@ class Messages extends React.Component {
     super()
     this.state = {
       socket: null,
-      message: '',
       author: null,
       messages: [],
       room: null
@@ -18,32 +18,49 @@ class Messages extends React.Component {
     this.sendMessage = this.sendMessage.bind(this);
   }
 
-  componentWillMount() {
-    this.socket = io('http://localhost:4155', {
+  async componentWillMount  () {
+    const socket = io('http://localhost:4155', {
       query: {
         roomId: location.pathname.slice(1)
       }
-    });
-    console.log('socket', this.socket)
-    axios.get('http://localhost:4155/api/chat/getMessages')
-      .then((res) => {
-        console.log('resss', res)
-        this.setState({
-          messages: res.data
-        })
-      })
-      .catch(() => {
-        console.log('error fetching messages')
-      })
+    });   
+    this.setState({ socket: socket, room: location.pathname.slice(1) });
 
-    this.setState({ socket: this.socket, room: location.pathname.slice(1) });
-    console.log('messages', this.state.messages)
+    try {
+       const data = await axios.get(`http://localhost:4155/api/chat/getMessages`)
+        this.setState({
+          messages: data.data
+        })
+      } 
+    catch (err) {
+        console.log('couldnt fetch err', err)
+      }
+      
+    socket.on('connect', () => {
+      socket.emit('client.ready', this.state.room)
+    })
+
+    socket.on('server.initialState', () => {
+      this.setState({socket})
+    })
+
+    socket.on('server.message', async (data) => {
+      try {
+        const lastmessage = await axios.get('http://localhost:4155/api/chat/getLastMessage')
+         this.setState({
+          messages: [...this.state.messages, lastmessage.data[0]]
+        })
+      }
+      catch (err) {
+        console.log('couldnt get last message', err)
+      }
+    })
   }
 
   sendMessage(e) {
     e.preventDefault();
-    this.setState({message: this.message})
-    this.socket.emit('client.message', {
+    this.setState({message: this.state.messages})
+    this.state.socket.emit('client.message', {
       author: this.props.active_user.displayName,
       authorImage: this.props.active_user.photoURL,
       message: this.state.message,
@@ -55,21 +72,33 @@ class Messages extends React.Component {
   render() {
     return (
       <div className="container">
-        <div className="card">
-            <div className="card-body">
-                <div className="card-title">Chat</div>
-                <hr/>
-                <div className="messages">
-
-                </div>
+      <div>
+      {this.state.messages.length > 0
+      ?
+      this.state.messages.map((message, i) => {
+        return (<div key={i}>
+          <li>
+          <img src={message.authorImage} />
+          <span>({message.author}) : {message.message} </span>
+          </li>
+        </div>)
+      })
+      :
+      null
+      }
+      </div>  
+      <div className="card">
+        <div className="card-title">Chat</div>
+          <hr/>
+            <div className="messages">
+              </div>
                 <div className="footer">
                     <br/>
                     <input type="text" placeholder="Message"  value={this.state.message} onChange={e => this.setState({message: e.target.value})}/>
                     <br/>
                     <button  onClick={(e) => this.sendMessage(e)}>Send</button>
                 </div>
-            </div>
-        </div>  
+          </div>  
       </div>
   );
 }
@@ -83,6 +112,3 @@ function mapStateToProps(state) {
 }
 
 export default connect(mapStateToProps)(Messages);
-
-
-//                              <input type="text" placeholder="Username" value={this.state.username} onChange={ev => this.setState({username: ev.target.value})} className="form-control"/>
