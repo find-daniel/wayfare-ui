788df32de5a0cc62819d54a5a5f52b5f83d7fe2a
Daniel Sanchez <atlow.archive@gmail.com>
2018-02-06 20:00:37 -0800

    [editUser] setup redux state, minor styling, fixed login redirects, setup uid in localstorage

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { render } from 'react-dom';
 import { createLogger } from 'redux-logger';
+import thunk from 'redux-thunk';
 import { applyMiddleware, createStore } from 'redux';
 import reducers from './reducers';
 // React Router
@@ -9,7 +10,7 @@ import reducers from './reducers';
 import App from './App.jsx';
 import './index.css'
 
-const middleWare = applyMiddleware(createLogger());
+const middleWare = applyMiddleware(thunk, createLogger());
 const store = createStore(reducers, middleWare);
 
 render(<App store={store} />, document.getElementById('app'));
--- client/src/reducers/index.js
+++ client/src/reducers/index.js
@@ -1,7 +1,9 @@
 import { combineReducers } from 'redux';
 import active_user from './active_user';
+import user_data from './user_data';
 // Import reducers
 
 export default combineReducers({
-  active_user: active_user
+  active_user: active_user,
+  user_data: user_data
 });
